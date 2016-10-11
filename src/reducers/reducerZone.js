23c5b3823d57ebf5afd87a1711954d4ea4322766
Ted Shaffer <ted@brightsign.biz>
2016-10-11 08:16:13 -0700

    bug fix

--- src/reducers/reducerZone.js
+++ src/reducers/reducerZone.js
@@ -4,16 +4,12 @@ import Zone from '../entities/zone';
 
 const emptyZone = new Zone("Untitled", "images", true);
 
-// export default function(state = emptyZone, {type, payload}) {
-export default function(state = emptyZone, action) {
+export default function(state = emptyZone, {type, payload}) {
 
-    console.log("poopoo");
-
-    switch (action.type) {
+    switch (type) {
         case NEW_ZONE:
-            // let {name, type, nonInteractive} = payload;
-            let {name, type, nonInteractive} = action.payload;
-            return new Zone(name, type, nonInteractive);
+            let {name, zoneType, nonInteractive} = payload;
+            return new Zone(name, zoneType, nonInteractive);
     }
 
     return state;
