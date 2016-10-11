3cc2167bdcab14f371f51d66e45ac7fc3189d32c
Ted Shaffer <ted@brightsign.biz>
2016-10-11 08:18:55 -0700

    return zoneId on creation of new zone.

--- src/reducers/reducerZone.js
+++ src/reducers/reducerZone.js
@@ -4,12 +4,16 @@ import Zone from '../entities/zone';
 
 const emptyZone = new Zone("Untitled", "images", true);
 
-export default function(state = emptyZone, {type, payload}) {
+export default function(state = emptyZone, action) {
+
+    const {type, payload} = action;
 
     switch (type) {
         case NEW_ZONE:
             let {name, zoneType, nonInteractive} = payload;
-            return new Zone(name, zoneType, nonInteractive);
+            let newZone = new Zone(name, zoneType, nonInteractive);
+            action.zoneId = newZone.id;
+            return newZone;
     }
 
     return state;
